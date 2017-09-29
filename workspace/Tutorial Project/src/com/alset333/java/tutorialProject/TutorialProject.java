0e010a473a8a10ecf618bccd9b31434a2febbf96
alset333 <alset333@icloud.com>
2017-09-28 20:20:23 -0400

    First Recording Done. Hello, World!

---
+++ workspace/Tutorial Project/src/com/alset333/java/tutorialProject/TutorialProject.java
@@ -0,0 +1,11 @@
+package com.alset333.java.tutorialProject;
+
+public class TutorialProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello, world!");
+
+	}
+
+}
